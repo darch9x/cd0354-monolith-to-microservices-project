d8299462f196cf812cd5e3f207a6c6c802a586fe
darch9x <37670152+darch9x@users.noreply.github.com>
2024-07-05 23:50:44 +0700

    init

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=dinhle
+export POSTGRES_PASSWORD=123456789
+# export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-udagram.cfewylglfrji.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=dinhle
+export AWS_BUCKET=arn:aws:s3:::prj3-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
